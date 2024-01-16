76d463294ca2393f705222a50428644a861226a1
James Lang <hi@thejameslang.com>
2024-01-15 20:12:42 -0600

    switch over to public npm package

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { Demo } from '@siliconvalleybank/dimension-example-component-library';
+
 export const DemoView = () => (
   <div>
     <h1>This is my application!</h1>
+    <Demo />
   </div>
 );
